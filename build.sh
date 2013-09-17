5c17d07c4a039753604ac2e536def55ecd487acd
aaronhayes <aaron.hayes92@gmail.com>
2013-09-17 19:40:47 +1000

    Fix build script error. Requires manual input to complete. Not a big deal.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ if [ $OUT -eq 0 ];then
 	echo "===== MAKING FS IMAGE ===="
 	rm -f 500K.img
 	dd if=/dev/zero of=500K.img bs=500K count=0 seek=1
-	y | mke2fs 500K.img
+	mke2fs 500K.img
 	echo ""
 	echo "==== MADE FS ON 500K.img ===="
 else
