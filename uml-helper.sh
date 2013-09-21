f07aef03c5b6242543b0fe6711fa4a4d241eda09
aaronhayes <aaron.hayes92@gmail.com>
2013-09-22 02:48:55 +1000

    updated helper scripts

--- uml-helper.sh
+++ uml-helper.sh
@@ -1,13 +1,19 @@
+#STARTING UML
 cd ~/COMP3301/dev/ass3/comp3301-s4233846/a3/
 ./uml-kernel ubda=rootfs.ext2 mem=128m umid=comp3301 eth0=daemon ubdb=500K.img
-mount none /mnt/host -t hostfs -o /home/students/s4233846
-cd /mnt/host/COMP3301/dev/ass3/comp3301-s4233846/a3/
-insmod ext3301.ko
-mount -t ext3301 -o key=0xAA /dev/ubdb /mnt/ext3301
 
+#MOUNTING FS
+mount none /mnt/host -t hostfs -o /home/students/s4233846
+#cd /mnt/host/COMP3301/dev/ass3/comp3301-s4233846/a3/
+insmod /mnt/host/COMP3301/dev/ass3/comp3301-s4233846/a3/ext3301.ko
+mount -t ext3301 -o debug -o key=0xAA /dev/ubdb /mnt/ext3301
 uml_mconsole comp3301
 config ubdb=500K.img
 
+#TO VIEW KERN_INFO printks
+dmesg
+
+#UNMOUNT/POWEROFF
 umount /mnt/ext3301
 rmmod ext3301
 #poweroff
